6cc9b14005af7abbd63325b39248565940fcea65
Ibratjon <mehmonaliyevibratjon4469@gmail.com>
2022-12-11 18:30:54 +0500

    Remove

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -25,76 +25,85 @@ function ModalComp({ isModal, setIsModal }) {
 
     const hendleClick = () => {
         try {
-           let newMass = product.products.map((s)=>{
-            if (s.id == isModal.id) {
-                return {
-                    ...s,
-                    title:title,
-                    desc:desc,
-                    price:price,
-                    count:count
+            let newMass = product.products.map((s) => {
+                if (s.id == isModal.id) {
+                    return {
+                        ...s,
+                        title: title,
+                        desc: desc,
+                        price: price,
+                        count: count
+                    }
+                } else {
+                    return s
                 }
-            }else{
-                return s
-            }
-           });
-           setProduct({...product, products: newMass})
-           setIsModal({isOpen:false})
-
-
+            });
+            setProduct({ ...product, products: newMass })
+            setIsModal({ isOpen: false })
         } catch (error) {
-        console.log(error);
+            console.log(error);
+        }
     }
-}
-return (
-    <>
 
-        <Modal open={isModal.isOpen} footer={false} onCancel={() => setIsModal({ isOpen: false })}>
-            <form className='p-4 ' onSubmit={hendleClick}>
+    const hendleRemove = (e)=>{
+        e.preventDefaul()
+        try {
+            let NewMass = product.products.filter(s => s.id == isModal.id)
+            setProduct({ ...product, products: NewMass })
+            setIsModal({ isOpen: false })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <>
 
-                <input
-                    className='form-control my-3 p-2'
-                    type="text"
-                    placeholder='Enter Title'
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
+            <Modal open={isModal.isOpen} footer={false} onCancel={() => setIsModal({ isOpen: false })}>
+                <form className='p-4 ' onSubmit={hendleClick}>
 
-                <input
-                    className='form-control my-3 p-2'
-                    type="text"
-                    placeholder='Enter Descripshin'
-                    value={desc}
-                    onChange={(e) => setDesc(e.target.value)}
-                />
+                    <input
+                        className='form-control my-3 p-2'
+                        type="text"
+                        placeholder='Enter Title'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
 
-                <input
-                    className='form-control my-3 p-2'
-                    type="number"
-                    placeholder='Enter Price'
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                />
+                    <input
+                        className='form-control my-3 p-2'
+                        type="text"
+                        placeholder='Enter Descripshin'
+                        value={desc}
+                        onChange={(e) => setDesc(e.target.value)}
+                    />
 
-                <input
-                    className='form-control my-3 p-2'
-                    type="number"
-                    placeholder='Enter Count'
-                    value={count}
-                    onChange={(e) => setCount(e.target.value)}
-                />
+                    <input
+                        className='form-control my-3 p-2'
+                        type="number"
+                        placeholder='Enter Price'
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
 
-                <div className='d-flex justify-content-between'>
-                    <button className='btn btn-danger p-1'>Remove</button>
-                    <button className='btn btn-warning p-1' onClick={hendleClick}>Edit</button>
-                </div>
+                    <input
+                        className='form-control my-3 p-2'
+                        type="number"
+                        placeholder='Enter Count'
+                        value={count}
+                        onChange={(e) => setCount(e.target.value)}
+                    />
 
-            </form>
+                    <div className='d-flex justify-content-between'>
+                        <button className='btn btn-danger p-1' onClick={hendleRemove}>Remove</button>
+                        <button className='btn btn-warning p-1' onClick={hendleClick}>Edit</button>
+                    </div>
 
-        </Modal>
-    </>
+                </form>
 
-)
+            </Modal>
+        </>
+
+    )
 }
 
 export default ModalComp
